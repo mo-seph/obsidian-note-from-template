809fdc065782c7b6f90b1fbb4776d07e606ee330
mo-seph <github@mo-seph.com>
2021-12-10 19:30:11 +0100

    Tidying, refactor, added create/open options

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import * as Mustache from 'mustache';
 import metadataParser from 'markdown-yaml-metadata-parser'
 import { BaseModal } from './BaseModal';
 import { FillTemplate } from './FillTemplate';
-import TemplateHelper, { ReplacementSpec, TemplateIdentifier } from './templates';
+import TemplateHelper, { CreateType, ReplacementSpec, ReplaceType, TemplateDefaults, TemplateIdentifier } from './templates';
 
 // Stop mustache from escaping HTML entities as we are generating Markdown
 Mustache.escape = function(text:string) {return text;};
@@ -16,16 +16,19 @@ Mustache.escape = function(text:string) {return text;};
  * - add a replacement string for what goes back into the text
  */
 
-interface FromTemplatePluginSettings {
+interface FromTemplatePluginSettings extends TemplateDefaults {
 	templateDirectory: string;
-	replaceSelection: boolean;
 	inputSplit: string;
 	config: string;
 }
 
 const DEFAULT_SETTINGS: FromTemplatePluginSettings = {
+	outputDirectory:"test",
+	inputFieldList:"title,body",
+	textReplacementTemplate:"[[{{title}}]]",
 	templateDirectory: 'templates',
-	replaceSelection: true,
+	replaceSelection: "always",
+	createOpen: "create",
 	inputSplit: "\\s+-\\s+",
 	config: '[]'
 }
@@ -33,10 +36,9 @@ const DEFAULT_SETTINGS: FromTemplatePluginSettings = {
 
 export interface ReplacementOptions {
 	editor:Editor;
-	createNote:boolean;
-	shouldReplaceSelection:boolean;
+	shouldReplaceSelection:ReplaceType
+	shouldCreateOpen:CreateType
 	willReplaceSelection:boolean;
-	openNote:boolean;
 }
 
 export default class FromTemplatePlugin extends Plugin {
@@ -54,29 +56,30 @@ export default class FromTemplatePlugin extends Plugin {
 	// Adds all the template commands - calls getTemplates which looks for files in the settings.templateDirectory
 	async addTemplates() {
 		const templates = await this.templates.getTemplates(this.settings.templateDirectory) || []
-		console.log("Got templates! ",templates)
+		console.log("Got templates: ",templates.map(c => c.path).join(", "))
 		templates.forEach(async t => {
-			const ts = (await t) as TemplateIdentifier
-			this.addCommand( {
-				id:ts.id,
-				name: ts.name,
-				editorCallback: async (editor, _ ) => { this.launchTemplate(editor,ts) }
-				
-			});
+			if( t ) {
+				const ts = (await t) as TemplateIdentifier
+				this.addCommand( {
+					id:ts.id,
+					name: ts.name,
+					editorCallback: async (editor, _ ) => { this.launchTemplate(editor,ts) }
+					
+				});
+			}
 		})
 	}
 
 	async launchTemplate(editor:Editor,ts:TemplateIdentifier) {
 		// Get the template text and the fields to fill in
 		const template = await this.templates.prepareTemplate(
-			ts,editor.getSelection(),this.settings.inputSplit)
+			ts,this.settings,editor.getSelection(),this.settings.inputSplit)
 
 		const options:ReplacementOptions = {
 			editor:editor,
-			createNote:true,
-			openNote:true,
-			shouldReplaceSelection:this.settings.replaceSelection,
-			willReplaceSelection:this.settings.replaceSelection,
+			shouldReplaceSelection:template.settings.shouldReplaceInput,
+			shouldCreateOpen:template.settings.shouldCreateOpen,
+			willReplaceSelection:true,
 		}
 		//This class does all the UI work
 		new FillTemplate(this.app,this,template,options).open();
@@ -86,16 +89,27 @@ export default class FromTemplatePlugin extends Plugin {
 	async templateFilled(spec:ReplacementSpec,options:ReplacementOptions) {
 		let [filledTemplate,replaceText] = await this.templates.fillOutTemplate(spec)
 
-		if( this.settings.replaceSelection && (spec.settings.textReplacementTemplate !== "none") ) {
+		console.log(spec)
+		console.log(options)
+		if( options.willReplaceSelection ) {
 			options.editor.replaceRange(replaceText,
 				options.editor.getCursor("from"), options.editor.getCursor("to"));
 		}
 
-		const filename =spec.settings.outputDirectory + "/" + spec.data['title'] + ".md" 
-		try {
-			this.app.vault.create(filename, filledTemplate)
-		} catch (error) {
-			alert("Couldn't create file: " + filename + "\n" + error.toString() )
+		if( options.shouldCreateOpen !== "none" ) {
+			const filetitle = spec.data['title'].replace(/[^a-zA-Z0-9 -:]/g,"") //Quick and dirty regex for usable titles
+			const filename =spec.settings.outputDirectory + "/" + filetitle + ".md" 
+			try {
+				const newFile = await this.app.vault.create(filename, filledTemplate)
+				if( options.shouldCreateOpen === "open") {
+					this.app.workspace.activeLeaf.openFile(newFile)
+				} 
+				else if( options.shouldCreateOpen === "open-pane") {
+					this.app.workspace.splitActiveLeaf().openFile(newFile)
+				}
+			} catch (error) {
+				alert("Couldn't create file: " + filename + "\n" + error.toString() )
+			}
 		}
 	}
 
@@ -141,21 +155,6 @@ class FromTemplateSettingTab extends PluginSettingTab {
 
 		containerEl.createEl('h2', {text: 'Note From Template Settings'});
 
-		/*
-		let nameText: TextComponent;
-		new Setting(contentEl)
-		.setName("Test")
-		.setDesc(("Testing stuff?"))
-		.addText((text) => {
-			nameText = text;
-			text.setValue("Hi")
-				.onChange((value) => {
-					console.log("New text: "+value)
-					//this.setValidationError(nameText, "invalid_name");
-				});
-		});
-		*/
-
 		const dirSetting = new Setting(containerEl)
 			.setName('Template Directory')
 			.setDesc('Directory to read templates from')
@@ -182,12 +181,56 @@ class FromTemplateSettingTab extends PluginSettingTab {
 		new Setting(containerEl)
 			.setName('Replace selection')
 			.setDesc('Should the current editor selection be replaced with a link to the title of the new Note?')
-			.addToggle(toggle => toggle
+			.addDropdown(toggle => toggle
+				.addOption("always","Always")
+				.addOption("sometimes","If Selected")
+				.addOption("never","Never")
 				.setValue(this.plugin.settings.replaceSelection)
 				.onChange(async (value) => {
-					this.plugin.settings.replaceSelection = value;
+					this.plugin.settings.replaceSelection = value as ReplaceType;
 					await this.plugin.saveSettings();
 				}));
+		new Setting(containerEl)
+		.setName('Create and Open Note')
+		.setDesc('Should a note be created and opened? If opened, in a pane?')
+		.addDropdown(toggle => toggle
+			.addOption("none","Don't create note")
+			.addOption("create","Create but don't open")
+			.addOption("open","Create and open in this pane")
+			.addOption("open-pane","Create and open in new pane")
+			.setValue(this.plugin.settings.createOpen)
+			.onChange(async (value) => {
+				this.plugin.settings.createOpen = value as CreateType;
+				await this.plugin.saveSettings();
+			}));
+		new Setting(containerEl)
+		.setName('Default output directory')
+		.setDesc('Where to put notes if they have not specified with {{template-output}}')
+		.addText(text => text
+			.setValue(this.plugin.settings.outputDirectory)
+			.onChange(async (value) => {
+				this.plugin.settings.outputDirectory = value;
+				await this.plugin.saveSettings();
+			}));
+		new Setting(containerEl)
+		.setName('Default replacement string')
+		.setDesc('What replacement string to use if the template has not specified using {{template-replacement}}')
+		.addText(text => text
+			.setValue(this.plugin.settings.textReplacementTemplate)
+			.onChange(async (value) => {
+				this.plugin.settings.textReplacementTemplate = value;
+				await this.plugin.saveSettings();
+			}));
+		new Setting(containerEl)
+		.setName('Default field list')
+		.setDesc('What fields to expect if they template does not specify with {{template-input}}')
+		.addText(text => text
+			.setValue(this.plugin.settings.inputFieldList)
+			.onChange(async (value) => {
+				this.plugin.settings.inputFieldList = value;
+				await this.plugin.saveSettings();
+			}));
+
 		new Setting(containerEl)
 			.setName('Selection split')
 			.setDesc('A regex to split up the input selection to fill in extra fields in the note creation box')
